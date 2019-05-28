14499adad36ca83c73d989eeb689d2b1376b5840
Moonkaman <nickbatmanglidj@gmail.com>
2019-05-28 15:30:15 -0600

    Finish pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
     Given a character pointer x (that points to an array of chars), and a
@@ -15,7 +16,15 @@
 */
 void string_copy(char *x, char *y)
 {
-
+  int i = 0;
+  while (*y != '\0')
+  {
+    x[i] = *y;
+    i++;
+    y++;
+  }
+  x[i] = '\0';
+  printf("%s\n", x);
 }
 
 /*
@@ -28,7 +37,24 @@ void string_copy(char *x, char *y)
 */
 char *find_char(char *str, int c)
 {
-
+  int found = 0;
+  while (*str != '\0')
+  {
+    if (*str == c)
+    {
+      found = 1;
+      break;
+    }
+    str++;
+  }
+  if (found)
+  {
+    return str;
+  }
+  else
+  {
+    return NULL;
+  }
 }
 
 /*
@@ -41,18 +67,44 @@ char *find_char(char *str, int c)
 */
 char *find_string(char *haystack, char *needle)
 {
-
+  int needLen = strlen(needle) - 1;
+  while (*haystack != '\0')
+  {
+    if (*haystack == *needle)
+    {
+      int found = 1;
+      for (int i = 0; i <= needLen; i++)
+      {
+        if (*(haystack + i) != *(needle + i))
+        {
+          found = 0;
+          break;
+        }
+      }
+      if (found)
+      {
+        return haystack;
+      }
+    }
+    haystack += 1;
+  }
+  return NULL;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    char *found_char = find_char("hello", 'e');
-    char *found_string = find_string("world", "or");
 
-    printf("Found char: %s\n", found_char);
-    printf("Found string: %s\n", found_string);
+  char buffer[1024];
 
-    return 0;
+  string_copy(buffer, "Hello!");
+
+  char *found_char = find_char("hello", 'e');
+  char *found_string = find_string("hey sponpbob, whats up spornbob hey sponge man", "spong");
+
+  printf("Found char: %s\n", found_char);
+  printf("Found string: %s\n", found_string);
+
+  return 0;
 }
 #endif
